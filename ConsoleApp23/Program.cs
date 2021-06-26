f442a04d6db977c7ec247cb51aa1f22e09bc4fd5
Pasha-kto003 <lukovnikov.pasha@gmail.com>
2021-06-26 21:56:00 +0300

    счетчик слов

--- ConsoleApp23/Program.cs
+++ ConsoleApp23/Program.cs
@@ -7,16 +7,22 @@ namespace ConsoleApp23
         static void Main(string[] args)
         {
             Console.WriteLine("Введите текст");
+            int count = 0;
             string text = Console.ReadLine();
             string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < words.Length; i++)
             {
+                count++;
                 if (i % 2 == 0)
                 {
                     char[] array = words[i].ToCharArray();
                     Array.Reverse(array);
                     words[i] = string.Join("", array);
                 }
+            }
+            if(count > 6)
+            {
+                Console.WriteLine($"Слишком много словв введено: {count}");
             }
             string alltext = string.Join(' ', words);
             Console.WriteLine(alltext);
